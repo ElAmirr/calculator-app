663a2dccc78c671ce2cd7b00577b24a4448849f0
ElAmirr <othmaniamir41@gmail.com>
2024-01-06 00:30:25 +0100

    add some logic to the js file

--- script.js
+++ script.js
@@ -1,51 +1,38 @@
-let displayResult = document.getElementById("display-result")
-let displayOperation = document.getElementById("display-operation")
-let result = 0
+let displayResult = document.getElementById("display-result");
+let displayOperation = document.getElementById("display-operation");
+let equalBtnState = false;
 
-const buttons = document.querySelectorAll('.nbr-btn')
+const buttons = document.querySelectorAll('.btn');
+const totalBtn = document.getElementById('total-btn')
+
 
 buttons.forEach(function (button) {
     button.addEventListener('click', function () {
-        const nbr = this.getAttribute('data-number')
-        if (displayResult.innerText === '0' || displayResult.innerText === '') {
-            displayResult.textContent=nbr
-        }else{
-            displayResult.textContent = displayResult.textContent+nbr;
-            }
-        
+        newNbrStatus = true;
+        const btnValue = this.getAttribute('data-number')
+        if (displayOperation.innerText === '0' || equalBtnState === true) {
+            displayOperation.textContent=btnValue;
+            displayResult.textContent=''
+            equalBtnState = false;
+        } else {
+            displayOperation.textContent = displayOperation.textContent+btnValue;
+        }
         this.style.boxShadow = '3px 3px 5px rgba(0, 0, 0, 0.2)';
         setTimeout(() => {
             this.style.boxShadow = '3px 3px 5px #131a24'
         }, 250)
-        console.log(result)
     })})
 
-const totalBtn = document.getElementById('total-btn')
 
 totalBtn.addEventListener('click', ()=>{
-    if (displayResult.textContent !=='0'){
-        result = result + parseInt(displayResult.textContent)
-        displayOperation.textContent+= displayResult.textContent
-    }
-    displayResult.textContent = result
-    console.log(result)
-    console.log(typeof(result))
-})
-
-const addBtn = document.getElementById('add-btn')
-addBtn.addEventListener('click', ()=>{
-    if (result === parseInt(displayResult.textContent)) {
-        return;
-    }
-    result = result + parseInt(displayResult.textContent)
-    displayOperation.textContent+= displayResult.textContent
-    displayOperation.textContent+= ' +'
-    displayResult.textContent = '0'
-    console.log(result)
-    console.log(typeof(result))
+    console.log(displayOperation.textContent)
+    console.log(typeof(displayOperation.textContent))
+    displayResult.textContent = eval(displayOperation.textContent)
+    equalBtnState = true;
 })
 
 const clearBtn = document.getElementById('clear-btn')
 clearBtn.addEventListener('click', ()=>{
-    displayResult.textContent='0'
+    displayOperation.textContent="0"
+    displayResult.textContent=''
 })
